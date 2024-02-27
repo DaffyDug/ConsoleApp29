6842c487fdbe533dae36c3ca65c5b9c4d0c85ee7
Diamont RED <motravasa5@gmail.com>
2024-02-27 18:36:37 +0700

    обновил

--- ConsoleApp29/InputText.cs
+++ ConsoleApp29/InputText.cs
@@ -3,24 +3,92 @@ using System.Text;
 
 class InputText
 {
-    private string _text;
-    public InputText(string text)
-    {
-        _text = text;
-    }
-    public string inputtext(string txt)
+    public string inputtext(string txt, StringBuilder _sb)
     {
         Console.Write("введиет текст: ");
         txt = Console.ReadLine();
+        while (true)
+        {
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. WriteLine");
+            string intvalue2 = Console.ReadLine();
+            if (intvalue2 == "1" || intvalue2 == "2")
+            {
+                if (intvalue2 == "1")
+                {
+                    _sb.Append(txt);
+                }
+                if (intvalue2 == "2")
+                {
+                    _sb.Append("\n" + txt);
+                }
+                break;
+            }
+            else
+            {
+                Console.WriteLine("ошибка\n");
+            }
+        }
         return txt;
     }
-    public void outputtext(StringBuilder stringBuilder)
+    public void outputtext(StringBuilder _sb)
     {
-        Console.WriteLine(stringBuilder);
+        if (_sb.Length > 0)
+        {
+            Console.WriteLine("ваш текст: " + _sb + "\n");
+        }
+        else
+        {
+            Console.WriteLine("ошибка\n");
+        }
     }
-    public void Clear_text(StringBuilder stringBuilder)
+    public void Clear_text(StringBuilder _sb)
     {
-        stringBuilder.Clear();
+        if (_sb.Length > 0)
+        {
+            Console.WriteLine("вы точно хотите удалить текст?");
+            Console.WriteLine("1. да");
+            Console.WriteLine("2. нет");
+            string intvalue3 = Console.ReadLine();
+            if (intvalue3 == "1")
+            {
+                _sb.Clear();
+                Console.WriteLine("\n");
+            }
+        }
+        else
+        {
+            Console.WriteLine("нельзя удалить то чего нет\n");
+        }
+    }
+    public void Replace_text(StringBuilder _sb)
+    {
+        if (_sb.Length > 0)
+        {
+            Console.WriteLine(_sb);
+            Console.WriteLine("какие строки вы хотите заменить ?");
+            string str = Console.ReadLine();
+            Console.WriteLine("на какие стоки вы хотите заменить?");
+            string str2 = Console.ReadLine();
+            Console.WriteLine($"вы хотите заменить '{str}' на '{str2}' \n все верно ? \n 1. да \n 2. нет \n");
+            string str3 = Console.ReadLine();
+            if (str3 == "1")
+            {
+                if (_sb.ToString().Contains(str))
+                {
+                    _sb.Replace(str, str2);
+                    Console.WriteLine(_sb + "\n");
+                }
+                else
+                {
+                    Console.WriteLine("таких строк либо нет, либо мы не можем заменить на эти строки\n");
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("нельзя изменить то чего нет\n");
+        }
     }
     public void Clear_scr()
     {
--- ConsoleApp29/Program.cs
+++ ConsoleApp29/Program.cs
@@ -1,138 +1,48 @@
-﻿using System;
-using System.Text;
-class Program
+﻿namespace HW_6_
 {
-
-
-
-
-
-
-
-
-    
-    static void Main(string[] args)
+    using System;
+    using System.Text;
+    class Program
     {
-        string stringtext = "";
-        InputText inputText = new InputText(stringtext);
-        StringBuilder stringBuilder = new StringBuilder();
-        while (true)
+        static void Main(string[] args)
         {
-            Console.WriteLine("1. введение текста");
-            Console.WriteLine("2. вывод на экран");
-            Console.WriteLine("3. очистить текст");
-            Console.WriteLine("4. замена строк");
-            Console.WriteLine("5. очистить экран");
-            string intvalue = Console.ReadLine();
-            if (intvalue == "1")
+            string stringtext = "";
+            InputText inputText = new InputText();
+            StringBuilder stringBuilder = new StringBuilder();
+            while (true)
             {
-                string txt = inputText.inputtext(stringtext);
-                while (true)
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("1. введение текста");
+                Console.WriteLine("2. вывод на экран");
+                Console.WriteLine("3. очистить текст");
+                Console.WriteLine("4. замена строк");
+                Console.WriteLine("5. очистить экран");
+                Console.WriteLine("6. закрыть программу");
+                string intvalue = Console.ReadLine();
+                if (intvalue == "1")
                 {
-                    Console.WriteLine("1. Write");
-                    Console.WriteLine("2. WriteLine");
-                    string intvalue2 = Console.ReadLine();
-                    if (intvalue2 == "1" || intvalue2 == "2")
-                    {
-                        if (intvalue2 == "1")
-                        {
-                            stringBuilder.Append(txt);
-                        }
-                        if (intvalue2 == "2")
-                        {
-                            stringBuilder.Append("\n" + txt);
-                        }
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("ошибка");
-                    }
+                    inputText.inputtext(stringtext, stringBuilder);
                 }
-            }
-            if (intvalue == "2")
-            {
-                if (stringBuilder.Length > 0)
+                if (intvalue == "2")
                 {
                     inputText.outputtext(stringBuilder);
                 }
-                else
+                if (intvalue == "3")
                 {
-                    Console.WriteLine("ошибка");
+                    inputText.Clear_text(stringBuilder);
                 }
-            }
-            if (intvalue == "3")
-            {
-                if (stringBuilder.Length > 0)
+                if (intvalue == "4")
                 {
-
-                    Console.WriteLine("вы точно хотите удалить текст?");
-                    Console.WriteLine("1. да");
-                    Console.WriteLine("2 или любая другая цифра. нет");
-                    string intvalue3 = Console.ReadLine();
-                    if (intvalue3 == "1")
-                    {
-                        inputText.Clear_text(stringBuilder);
-                    }
-                    else
-                    {
-                        continue;
-                    }
+                    inputText.Replace_text(stringBuilder);
                 }
-                else
+                if (intvalue == "5")
                 {
-                    Console.WriteLine("нельзя удалить то чего нет");
+                    inputText.Clear_scr();
                 }
-            }
-            if (intvalue == "4")
-            {
-                if (stringBuilder.Length > 0)
+                if (intvalue == "6")
                 {
-                    Console.WriteLine(stringBuilder);
-                    Console.WriteLine("какие строки вы хотите заменить ?");
-                    string str = Console.ReadLine();
-                    Console.WriteLine("на какие стоки вы хотите заменить?\n");
-                    string str2 = Console.ReadLine();
-                    Console.WriteLine($"вы хотите заменить {str} на {str2} \n все верно ? \n 1. да 2.нет \n");
-                    string str3 = Console.ReadLine();
-                    if (str3 == "1")
-                    {
-
-                        if (stringBuilder.ToString().Contains(str))
-                        {
-                            stringBuilder.Replace(str, str2);
-                            Console.WriteLine(stringBuilder);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                            
-                        }
-                        else
-                        {
-                            Console.WriteLine("таких строк либо нет, либо мы не можем заменить на эти строки");
-                        }
-                    }
-                    else
-                    {
-                        continue;
-                    }
+                    break;
                 }
-            }
-            if (intvalue == "5")
-            {
-                inputText.Clear_scr();
             }
         }
     }
