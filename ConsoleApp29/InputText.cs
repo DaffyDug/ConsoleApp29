ba46121557c8704b1e477004f46e547acab2ee0b
Александр Александр <stripalol@inbox.ru>
2024-02-27 12:56:36 +0700

    супер игра

---
+++ ConsoleApp29/InputText.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+class InputText
+{
+    private string _text;
+    public InputText(string text)
+    {
+        _text = text;
+    }
+    public string inputtext(string txt)
+    {
+        Console.Write("введиет текст: ");
+        txt = Console.ReadLine();
+        return txt;
+    }
+    public void outputtext(StringBuilder stringBuilder)
+    {
+        Console.WriteLine(stringBuilder);
+    }
+    public void Clear_scr(StringBuilder stringBuilder)
+    {
+        stringBuilder.Clear();
+    }
+}
--- ConsoleApp29/Program.cs
+++ ConsoleApp29/Program.cs
@@ -1,79 +1,110 @@
 ﻿using System;
-
+using System.Text;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
-    }
-}
-interface IDamageble
-{
-    void Damageble(float x);
-}
-class Player : IDamageble
-{
-    private float _heath;
-    private void Die()
-    {
-        Console.WriteLine("aaaaaaaaaa");
-    }
-    public void Damageble(float x)
-    {
-        if (_heath <= 0)
+        string stringtext = "";
+        InputText inputText = new InputText(stringtext);
+        StringBuilder stringBuilder = new StringBuilder();
+        while (true)
         {
-            _heath = 0;
-            Console.WriteLine(_heath);
-            Die();
-        }
-        else
-        {
-            _heath -= x;
-            Console.WriteLine(_heath);
-        }
-    }
-}
-class Wall : IDamageble
-{
-    private float _heath;
-    private void Die()
-    {
-        Console.WriteLine("tttttttttttttttxxxxxxxxxxx");
-    }
-    public void Damageble(float x)
-    {
-        if (_heath <= 0)
-        {
-            _heath = 0;
-            Console.WriteLine(_heath);
-            Die();
-        }
-        else
-        {
-            _heath -= x;
-            Console.WriteLine(_heath);
-        }
-    }
-}
-class Car : IDamageble
-{
-    private float _heath;
-    private void Die()
-    {
-        Console.WriteLine("cccccxxxxxrrrrr");
-    }
-    public void Damageble(float x)
-    {
-        if (_heath <= 0)
-        {
-            _heath = 0;
-            Console.WriteLine(_heath);
-            Die();
-        }
-        else
-        {
-            _heath -= x;
-            Console.WriteLine(_heath);
+            Console.WriteLine("1. введение текста");
+            Console.WriteLine("2. вывод на экран");
+            Console.WriteLine("3. очистить текст");
+            Console.WriteLine("4. замена строк");
+            Console.WriteLine("5. очистить экран");
+            string intvalue = Console.ReadLine();
+            if (intvalue == "1")
+            {
+                string txt = inputText.inputtext(stringtext);
+                while (true)
+                {
+                    Console.WriteLine("1. Write");
+                    Console.WriteLine("2. WriteLine");
+                    string intvalue2 = Console.ReadLine();
+                    if (intvalue2 == "1" || intvalue2 == "2")
+                    {
+                        if (intvalue2 == "1")
+                        {
+                            stringBuilder.Append(txt);
+                        }
+                        if (intvalue2 == "2")
+                        {
+                            stringBuilder.Append("\n" + txt);
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("ошибка");
+                    }
+                }
+            }
+            if (intvalue == "2")
+            {
+                if (stringBuilder.Length > 0)
+                {
+                    inputText.outputtext(stringBuilder);
+                }
+                else
+                {
+                    Console.WriteLine("ошибка");
+                }
+            }
+            if (intvalue == "3")
+            {
+                if (stringBuilder.Length > 0)
+                {
+
+                    Console.WriteLine("вы точно хотите удалить текст?");
+                    Console.WriteLine("1. да");
+                    Console.WriteLine("2 или любая другая цифра. нет");
+                    string intvalue3 = Console.ReadLine();
+                    if (intvalue3 == "1")
+                    {
+                        inputText.Clear_scr(stringBuilder);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("нельзя удалить то чего нет");
+                }
+            }
+            if (intvalue == "4")
+            {
+                if (stringBuilder.Length > 0)
+                {
+                    Console.WriteLine(stringBuilder);
+                    Console.WriteLine("какие строки вы хотите заменить ?");
+                    string str = Console.ReadLine();
+                    Console.WriteLine("на какие стоки вы хотите заменить?\n");
+                    string str2 = Console.ReadLine();
+                    Console.WriteLine($"вы хотите заменить {str} на {str2} \n все верно ? \n 1. да 2.нет \n");
+                    string str3 = Console.ReadLine();
+                    if (str3 == "1")
+                    {
+
+                        if (stringBuilder.ToString().Contains(str))
+                        {
+                            stringBuilder.Replace(str, str2);
+                            Console.WriteLine(stringBuilder);
+                        }
+                        else
+                        {
+                            Console.WriteLine("таких строк либо нет, либо мы не можем заменить на эти строки");
+                        }
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
         }
     }
 }
